d4860fa70abbf63a78d762401f78bb0c13f769de
ayumu-1212 <abe.ayumu.infratop@gmail.com>
2019-06-25 14:05:08 +0900

    Initial Commit Message

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '5778e9ab1df8c9fb0b441d7e77f71769e65daaf00dafcf30fc5ce400f08c51c77bacd127cda329a934470fd507e118ff1a31cbaa86005af2226e95fa02114d0e'
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+
+  get "/" => 'home#show', as: "home"
+  get 'home/about'
+  devise_for :users
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :books
+  resources :users, only: [:show, :index, :edit, :update]
+end
